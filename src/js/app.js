26e299fac0927eecf11ab74f9eef5eaa9f63dfc1
Rui Ma <mrsckd3035@gmail.com>
2020-05-22 13:29:52 -0500

    Create a function to fecth map API for search

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,26 @@
 const startingLocation = document.querySelector('.origin-form');
+const lockInWinnipeg = '-97.325875, 49.766204, -96.953987, 49.99275';
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHJzbGltcmVhcGVycnVpIiwiYSI6ImNrYTVpazZ6YzAwajYzZ21nNHlmY2VtMDkifQ.6XvTvzaHaHxeGag0GKOs4w';
+
+function displayStartingLocation(query) {
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${mapboxgl.accessToken}&limit=10&bbox=${lockInWinnipeg}`)
+    .then(resp => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error ('No results found');
+      }
+    })
+    .then(searchResults => {
+      let arrayOfResults;
+      arrayOfResults = searchResults.features;
+      console.log(arrayOfResults);
+    });
+}
 
 startingLocation.addEventListener('submit', function(e) {
   e.preventDefault();
   const input = e.target.querySelector('input');
-  console.log(input.value);
+  displayStartingLocation(input.value);
+  input.value = "";
 })
